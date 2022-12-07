b54096d8c60df8e42db8eb40ffc7f94e11d4551f
Аркадий <you@example.com>
2022-12-07 20:03:55 +0400

    First homework

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+// Console.WriteLine ("Input first number");
+// int i = int.Parse(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int j = int.Parse(Console.ReadLine());
+// if (i > j)
+// {
+//   Console.WriteLine("First number is more than second");
+// }
+// else if (i < j)
+// {
+//   Console.WriteLine("Second number is more than first");
+// }
+// else
+// {
+//   Console.WriteLine("First and second numbers are equal");
+// }  
+
+// Console.WriteLine ("Input first number");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine ("Input second number");
+// int b = int.Parse(Console.ReadLine());
+// Console.WriteLine("Input third number");
+// int c = int.Parse(Console.ReadLine());
+// int max;
+// if (b > a)
+// {
+//     max = b;
+// }
+// else max = a;
+// if (c > max) 
+// {
+//       max = c;
+// }
+// Console.WriteLine($"Max number is {max}");
+
+//  Console.WriteLine("Input number");
+//  int x = int.Parse(Console.ReadLine());
+//  if (x % 2 == 0)
+//  {
+//    Console.WriteLine($"Number {x} is even");
+//  }
+//  else Console.WriteLine($"Number {x} is odd");
+
+// Console.WriteLine("Input integer number more than 1");
+// int x = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Even numbers from 1 to {x} are:");
+// int i = 2;
+//   while (i <= x )
+//   {
+//   Console.WriteLine(i);
+//   i += 2;
+//   }
