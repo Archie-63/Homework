79d9b56d896c271650b39de4295601d63959ee81
Аркадий <you@example.com>
2022-12-22 00:24:28 +0400

    Homework9 +54 & 56 tasks

--- Program.cs
+++ Program.cs
@@ -285,3 +285,155 @@
 // Console.WriteLine("Average values of array columns are:");
 // ColumnAverage(Array);
 
+// int ReadInt()
+// {
+// Console.WriteLine("Input numbers");
+// int m = int.Parse(Console.ReadLine());
+// return m;
+// }
+// int [,] MakeFillArray(int Length1, int Length2)
+// {
+// int[,] arr = new int[Length1, Length2];
+//   for(int i = 0; i < Length1; i++)
+//   {
+//     for(int j = 0; j < Length2; j++)
+//     arr[i, j] = new Random().Next(-100,101);
+//   }
+//   return arr;
+// }
+// void PrintArray(int[,] Arr)
+// {
+//     for(int i = 0; i < Arr.GetLength(0); i++)
+//   {
+//     for(int j = 0; j < Arr.GetLength(1); j++)      
+//     Console.Write($"{Arr[i,j]} ");
+//     Console.WriteLine();
+//   }
+// }
+// void UpdateArray(int [,] Arr)
+// {
+//    int Temp =0;
+//    for(int k = 0; k < Arr.GetLength(0); k++)
+//   {
+//      for(int j = 0; j<Arr.GetLength(1); j++)
+//      {
+//        for(int i = j+1; i<Arr.GetLength(1); i++)
+//        {
+//          if(Arr[k,i] > Arr[k,j])
+//              {
+//                Temp = Arr[k,i];
+//                Arr[k,i] = Arr[k,j];
+//                Arr[k,j] = Temp;
+//              }
+//        }
+//        Console.Write($"{Arr[k,j]} ");
+//      }
+//     Console.WriteLine();  
+//   }
+//  }     
+
+// int[,] Array = MakeFillArray(ReadInt(),ReadInt());
+// PrintArray(Array);
+// Console.WriteLine();
+// UpdateArray(Array);
+
+// int ReadInt()
+// {
+//   Console.WriteLine("Input numbers");
+//  int m = int.Parse(Console.ReadLine());
+//  return m;
+// }
+// int [,] MakeFillArray(int Length1, int Length2)
+// {
+//   int[,] arr = new int[Length1, Length2];
+//   for(int i = 0; i < Length1; i++)
+//   {
+//     for(int j = 0; j < Length2; j++)
+//     arr[i, j] = new Random().Next(-100,101);
+//   }
+//   return arr;
+// }
+// void PrintArray(int[,] Arr)
+// {
+//   for(int i = 0; i < Arr.GetLength(0); i++)
+//  {
+//   for(int j = 0; j < Arr.GetLength(1); j++)      
+//   Console.Write($"{Arr[i,j]} ");
+//   Console.WriteLine();
+//  }
+// }
+// void FindMinRowSum(int [,] Arr)
+// {
+//    int[] row = new int[Arr.GetLength(0)];
+//    int Min = row[0];
+//    int MinIndex = 0;
+//    for(int i = 0; i < Arr.GetLength(0); i++)
+//   {
+//     int Sum =0;
+//     for(int j = 0; j < Arr.GetLength(1); j++)
+//    {
+//     row[i] = Sum+=Arr[i, j];
+//    }
+//    if(row[i] < Min)
+//    {
+//     Min = row[i];
+//     MinIndex = i;
+//    }
+//   }
+//     Console.Write($"Min sum cells row from top  is {MinIndex+1} ");
+// }     
+// int[,] Array = MakeFillArray(ReadInt(),ReadInt());
+// PrintArray(Array);
+// Console.WriteLine();
+// FindMinRowSum(Array);
+
+// int ReadInt()
+// {
+//   Console.WriteLine("Input numbers, first bigger than second");
+//  int m = int.Parse(Console.ReadLine());
+//  return m;
+// }
+// void ListBetweenNumbers(int n, int m)
+// {
+//    if(n>m)
+//   {
+//    Console.Write(m+ ", ");
+//    ListBetweenNumbers(n, m+1);
+//   }
+//   else  Console.Write(n); 
+// }
+// ListBetweenNumbers(ReadInt(),ReadInt());
+
+// int SumBetweenNumbers(int m, int n)
+// {
+//   if(m==n) return n;
+//   else return SumBetweenNumbers(m,n-1)+n;
+// }
+// int ReadInt()
+// {
+//   Console.WriteLine("Input numbers, first smaller than second");
+//  int m = int.Parse(Console.ReadLine());
+//  return m;
+// }
+// Console.WriteLine(SumBetweenNumbers(ReadInt(),ReadInt()));
+
+//   uint Ackerman (uint m, uint n)
+//   {
+//     if(m==0)
+//     {
+//     return n+1;
+//     }
+//     else if(n==0 && m !=0)
+//     {
+//     return Ackerman(m-1, 1);
+//     } 
+//     else  return Ackerman(m-1, Ackerman(m, n-1));
+//   }
+//   uint ReadInt()
+//   {
+//     Console.WriteLine("Input numbers");
+//   uint m = uint.Parse(Console.ReadLine());
+//   return m;
+//   }
+//   Console.WriteLine(Ackerman (ReadInt(), ReadInt()));
+
